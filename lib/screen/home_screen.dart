590a6f15db46cdc2cca4645e3d1788850c458557
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-11-26 17:16:34 +0900

    Feat: Practice family modifier

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_app/layout/default_layout.dart';
+import 'package:flutter_riverpod_app/screen/family_modifier_screen.dart';
 import 'package:flutter_riverpod_app/screen/future_provider_screen.dart';
 import 'package:flutter_riverpod_app/screen/state_notifier_provider_screen.dart';
 import 'package:flutter_riverpod_app/screen/state_provider_screen.dart';
@@ -53,6 +54,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: Text('FamilyModifierScreen'),
           ),
         ],
       ),
