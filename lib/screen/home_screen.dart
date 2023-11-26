11443033260e887cef0263353dfbc11d9f8da0f2
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-11-26 17:25:12 +0900

    Feat: Practice auto dispose modifier

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_app/layout/default_layout.dart';
+import 'package:flutter_riverpod_app/screen/auto_dispose_modifier_screen.dart';
 import 'package:flutter_riverpod_app/screen/family_modifier_screen.dart';
 import 'package:flutter_riverpod_app/screen/future_provider_screen.dart';
 import 'package:flutter_riverpod_app/screen/state_notifier_provider_screen.dart';
@@ -64,6 +65,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => AutoDisposeModifierScreen(),
+                ),
+              );
+            },
+            child: Text('AutoDisposeModifierScreen'),
           ),
         ],
       ),
