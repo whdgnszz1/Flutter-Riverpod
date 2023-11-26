443bb1851f3faa29528bbae62a848ef5d5eb7f5b
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-11-26 16:41:08 +0900

    Feat: Practice state notifier provider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_app/layout/default_layout.dart';
+import 'package:flutter_riverpod_app/screen/state_notifier_provider_screen.dart';
 import 'package:flutter_riverpod_app/screen/state_provider_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -20,6 +21,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StateProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StateNotifierProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StateNotifierProviderScreen'),
           ),
         ],
       ),
